7fc24adcc2327b3abbb7eeb0331701cc07aea009
vij_kes <97831607+vijkes@users.noreply.github.com>
2023-07-07 09:05:38 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,11 +1,11 @@
 variable "key_name" {
   type        = string
   description = "The name for ssh key, used for aws_launch_configuration"
-  default     = "devops-demo-01"
+  default     = "terraformkey2"
 }
 
 variable "cluster_name" {
   type        = string
   description = "The name of AWS ECS cluster"
-  default     = "terraform_workshop_cluster"
+  default     = "terraform_colortest_cluster"
 }
