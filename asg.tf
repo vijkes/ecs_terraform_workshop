13611afd1b46e94a0f1652e8ed148245c28a7bb9
vij_kes <97831607+vijkes@users.noreply.github.com>
2023-07-07 09:50:12 +0530

    Update asg.tf

--- asg.tf
+++ asg.tf
@@ -61,9 +61,9 @@ EOF
 resource "aws_autoscaling_group" "asg" {
   name                      = "test-asg"
   launch_configuration      = aws_launch_configuration.lc.name
-  min_size                  = 3
+  min_size                  = 1
   max_size                  = 4
-  desired_capacity          = 3
+  desired_capacity          = 2
   health_check_type         = "ELB"
   health_check_grace_period = 300
   vpc_zone_identifier       = module.vpc.public_subnets
