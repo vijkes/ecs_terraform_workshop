7123c91b2f01ef86a48892c678a3e2a38565a0a0
vij_kes <97831607+vijkes@users.noreply.github.com>
2023-07-07 09:07:21 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,12 @@
 provider "aws" {
-  region  = "us-east-1"
-  shared_credentials_file = "$HOME/.aws/credentials" # default
-  profile = "default" # you may change to desired profile
-  version = "~> 2.63"
+  profile = "default"
+
 }
 
 
 terraform {
   backend "s3" {
-    bucket = "ecsworkshopbucket00"
+    bucket = "ecscolortestbucket008340"
     key    = "state/terraform.tfstate"
     region = "us-east-1"
   }
